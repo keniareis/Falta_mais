5a3878b45f696cfe6425e2ab07cfd0a8577f7fc9
keniareis <keniapubg@gmail.com>
2024-12-02 00:19:35 -0300

    feat: connect to MongoDB and add Discipline model

---
+++ backend/src/database/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const connectDb = async () => {
+    console.log("Wait connecting to the database");
+    try {
+        await mongoose.connect("", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+
+        });
+        console.log('Database connect!');
+    }catch(error){
+        console.error('Error :', error);
+        process.exit(1);
+    }
+}
+
+export default connectDb;
