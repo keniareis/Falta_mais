43f454014c8d7ae3b522570ca59ed306717577fa
keniareis <keniapubg@gmail.com>
2024-12-18 11:04:13 -0300

    feat: encrypting passwords

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
     name: { 
@@ -15,8 +16,14 @@ const UserSchema = new mongoose.Schema({
     
     password: { 
         type: String, 
-        required: true
+        required: true,
+        select: false
     }
+});
+
+UserSchema.pre('save', async function (next){
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
 const User = mongoose.model("User", UserSchema);
