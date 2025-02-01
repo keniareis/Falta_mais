291c91462b97b9b7b8d2e8f076663881ffc1de9e
keniareis <keniapubg@gmail.com>
2025-01-31 21:15:41 -0300

    add: middleware to authorization token

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -4,7 +4,28 @@ import jwt from 'jsonwebtoken';
 dotenv.config();
 
 export const authMiddleware = (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if(!authorization){
+        return res.send(401);
+    }
+
+    const parts = authorization.split(" ");
+
+    if(parts.length !== 2){
+        return res.send(401);
+    }
+
+    const {schema,  token} = parts;
     
+    if(schema !== "Bearer"){
+        return res.send(401);
+    }
+
+    jwt.verify(token, process.env.SECRET_JWT, (error, decoded) => {
+        console.log(error);
+        console.log(decoded);
+    });
 
     next();
 }
